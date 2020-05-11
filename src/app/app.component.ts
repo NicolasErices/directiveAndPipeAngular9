ab9cdfb30b1a92d38fb4c7cc88008204b59e1c32
carlos erices mendez <carlos.erices@bci.cl>
2020-05-10 20:48:04 -0400

    test with pipes angular 9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'directiveAndPipeAngular9';
+  constructor(){}
+  nombre: string = 'Carlos Erices Mendez';
+  nombre2: string = 'nombre ejemplo capitalizado';
+  arreglo = [1,2,3,4,5,6,7,8,9];
+  PI: number = Math.PI;
+  porcentaje: number = 0.12345;
+  salari: number = 1234.5
+  hero = {
+    nombre: "Iron Man",
+    poder: "Dinero",
+    diner: {
+      monto: "1000000",
+    }
+  }
+  fecha:Date = new Date();
+  valorPromesa = new Promise<string>((resolve)=>{
+    setTimeout(()=>{
+      resolve("la promesa se resolvio")
+    }, 4500)
+  })
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+registerLocaleData(localeEs)
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'LOCALE_ID',
+      useValue: 'es'
+    }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
